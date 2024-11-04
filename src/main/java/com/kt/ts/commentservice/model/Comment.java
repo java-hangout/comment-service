318b928b45fa1d916e789c7a9325954649d170f1
java-hangout <contacts.veeresh@gmail.com>
2024-11-04 09:10:10 +0530

    updated logic

--- src/main/java/com/kt/ts/commentservice/model/Comment.java
+++ src/main/java/com/kt/ts/commentservice/model/Comment.java
@@ -3,15 +3,16 @@ package com.kt.ts.commentservice.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Document(collection = "comments")
 public class Comment {
     @Id
     private String id;
     private String ticketId; // Reference to the Ticket
-    private String createdBy;   // Reference to the User
-    private String content;
-
-    // Getters and Setters
+    private String userId;   // Reference to the User
+    private String comment;
+    private Date createdDate;
 
     public String getId() {
         return id;
@@ -29,19 +30,27 @@ public class Comment {
         this.ticketId = ticketId;
     }
 
-    public String getCreatedBy() {
-        return createdBy;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
-    public String getContent() {
-        return content;
+    public String getComment() {
+        return comment;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
     }
 }
